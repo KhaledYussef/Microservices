37670cf9928ede8dbb96e57f8c88e5b5fb6c2b53
Khaled Yussef <theshadow.khaled@gmail.com>
2024-09-25 12:19:18 +0300

    Ocelot

---
+++ Ocelot/Writer/Controllers/WritersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Reflection;
+
+using Writer.Services;
+
+namespace Writer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WritersController(IWriterRepository writerRepository) : ControllerBase
+    {
+        private readonly IWriterRepository _writerRepository = writerRepository;
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Models.Writer writer)
+        {
+            var result = _writerRepository.Insert(writer);
+
+            return Created($"/get/{result.Id}", result);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_writerRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var result = _writerRepository.Get(id);
+            Thread.Sleep(2000);
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
